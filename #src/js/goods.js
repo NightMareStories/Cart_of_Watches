c659ba10eb5cb659eb3c8c161243a15ac990e4ba
Poteryaev Aleksandr <sp202037@yandex.ru>
2024-06-29 18:35:50 +0300

    Updated to v.1.1.2
    Fixed image paths.

--- #src/js/goods.js
+++ #src/js/goods.js
@@ -2,43 +2,43 @@ const cart = {
     "p92779": {
         "name": "Wrist Watch CASIO G-2900F-8VER",
         "url": "#",
-        "image": "../img/casio-g-2900f-8ver-img.jpg",
-        "imageWebp": "../img/casio-g-2900f-8ver-img.webp",
+        "image": "./img/casio-g-2900f-8ver-img.jpg",
+        "imageWebp": "./img/casio-g-2900f-8ver-img.webp",
         "price": 71.55
     },
     "p93039": {
         "name": "Wrist Watch CASIO AE-1000W-1AVEF",
         "url": "#",
-        "image": "../img/casio-ae-1000w-1avef-img.jpg",
-        "imageWebp": "../img/casio-ae-1000w-1avef-img.webp",
+        "image": "./img/casio-ae-1000w-1avef-img.jpg",
+        "imageWebp": "./img/casio-ae-1000w-1avef-img.webp",
         "price": 35.64
     },
     "p63553250": {
         "name": "Wrist Watch CASIO W-800H-1AVES",
         "url": "#",
-        "image": "../img/casio-w-800h-1aves-img.jpg",
-        "imageWebp": "../img/casio-w-800h-1aves-img.webp",
+        "image": "./img/casio-w-800h-1aves-img.jpg",
+        "imageWebp": "./img/casio-w-800h-1aves-img.webp",
         "price": 26.54
     },
     "p93127": {
         "name": "Wrist Watch CASIO EF-552-1AVEF",
         "url": "#",
-        "image": "../img/casio-ef-552-1avef-img.jpg",
-        "imageWebp": "../img/casio-ef-552-1avef-img.webp",
+        "image": "./img/casio-ef-552-1avef-img.jpg",
+        "imageWebp": "./img/casio-ef-552-1avef-img.webp",
         "price": 124.76
     },
     "p79946990": {
         "name": "Wrist Watch CASIO EF-527D-1AVEF",
         "url": "#",
-        "image": "../img/casio-ef-527d-1avef-img.jpg",
-        "imageWebp": "../img/casio-ef-527d-1avef-img.webp",
+        "image": "./img/casio-ef-527d-1avef-img.jpg",
+        "imageWebp": "./img/casio-ef-527d-1avef-img.webp",
         "price": 183.95
     },
     "p6533206": {
         "name": "Wrist Watch CASIO SGW-100-2BER",
         "url": "#",
-        "image": "../img/casio-sgw-100-2ber-img.jpg",
-        "imageWebp": "../img/casio-sgw-100-2ber-img.webp",
+        "image": "./img/casio-sgw-100-2ber-img.jpg",
+        "imageWebp": "./img/casio-sgw-100-2ber-img.webp",
         "price": 109.15
     },
 }
